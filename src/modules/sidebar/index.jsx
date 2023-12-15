20c92b75ba0c3849d1d72a57f361076f2e4ea9af
RishikeshSV <rishuvidyarthi@gmail.com>
2023-12-16 01:57:29 +0530

    added import export buttons

--- src/modules/sidebar/index.jsx
+++ src/modules/sidebar/index.jsx
@@ -3,26 +3,38 @@ import { Items } from "./SidebarData";
 const Sidebar = ({ setDraggedItem }) => {
   return (
     <div className="sidebar pd-x-20">
-      <div className="sidebar-header">BLOCKS</div>
-      <div className="sidebar-items">
-        {Items.map((item) => (
-          <div
-            key={`Item-${item.Id}`}
-            className="sidebar-item d-flex"
-            draggable //enable div as a draggable
-            onDragStart={
-              (e) =>
-                setDraggedItem({
-                  ...item,
-                  x: e.clientX - e.currentTarget.getBoundingClientRect().left,
-                  y: e.clientY - e.currentTarget.getBoundingClientRect().top,
-                }) //calculate the offset
-            } //item being dragged
-          >
-            <i className="fa-solid fa-grip fa-rotate-90" />
-            <div className="pd-l-10">{item.Name} </div>
-          </div>
-        ))}
+      <div className="sidebar-body">
+        <div className="sidebar-header">BLOCKS</div>
+        <div className="sidebar-items">
+          {Items.map((item) => (
+            <div
+              key={`Item-${item.Id}`}
+              className="sidebar-item d-flex"
+              draggable //enable div as a draggable
+              onDragStart={
+                (e) =>
+                  setDraggedItem({
+                    ...item,
+                    x: e.clientX - e.currentTarget.getBoundingClientRect().left,
+                    y: e.clientY - e.currentTarget.getBoundingClientRect().top,
+                  }) //calculate the offset
+              } //item being dragged
+            >
+              <i className="fa-solid fa-grip fa-rotate-90" />
+              <div className="pd-l-10">{item.Name} </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="sidebar-footer">
+        <div className="options">
+          <i class="fa-solid fa-upload pd-r-8" />
+          IMPORT
+        </div>
+        <div className="options">
+          <i class="fa-solid fa-download pd-r-8" />
+          EXPORT
+        </div>
       </div>
     </div>
   );
