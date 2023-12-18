dbdaf61a835017feac477d0571b1c975d320414a
RishikeshSV <rishuvidyarthi@gmail.com>
2023-12-18 22:49:59 +0530

    implemented drag and select items inside canvas

--- src/modules/canvas/index.jsx
+++ src/modules/canvas/index.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ConfigureElement from "./ConfigureElement";
 
 const Canvas = ({ draggedItem = {}, setDraggedItem, items = [], setItems }) => {
-  const [activeItem, setActiveItem] = useState({}); //the item that was just dragged that is not saved on the canvas yet
+  const [activeItem, setActiveItem] = useState({}); //item that will get edited
 
   const [openConfigurePopup, setOpenConfigurePopup] = useState(false);
 
-  const [selectedItem, setSelectedItem] = useState({}); //the selected item from the canvas that is already saved (to edit or delete)
+  const [selectedItems, setSelectedItems] = useState([]); //the selected item from the canvas that is already saved (to edit or delete)
+
+  const canvasRef = useRef(null);
+  const [selectionArea, setSelectionArea] = useState({
+    start: { x: 0, y: 0 },
+    end: { x: 0, y: 0 },
+  });
+  const [isDragging, setIsDragging] = useState(false);
 
   const handleDrop = (e) => {
     //save coordinates and the dragged item in the canvas
     e.preventDefault();
-    const canvasRect = e.currentTarget.getBoundingClientRect();
+    const canvasRect = canvasRef.current.getBoundingClientRect();
     // Calculate the position relative to the canvas
     const x = e.clientX - draggedItem.x - canvasRect.left;
     const y = e.clientY - draggedItem.y - canvasRect.top;
@@ -24,12 +31,13 @@ const Canvas = ({ draggedItem = {}, setDraggedItem, items = [], setItems }) => {
         updatedItem,
         ...items.slice(index + 1),
       ]);
-      setSelectedItem(updatedItem);
+      setSelectedItems([updatedItem]);
     } else {
       //new item, add it to canvas after user sets up the configuration
       setActiveItem({
         Id: items.length,
         name: draggedItem.Name,
+        type: draggedItem.Type,
         position: { x, y },
       });
       setOpenConfigurePopup(true);
@@ -37,72 +45,136 @@ const Canvas = ({ draggedItem = {}, setDraggedItem, items = [], setItems }) => {
   };
 
   const handleKeyDown = (key) => {
-    if (Object.keys(selectedItem).length) {
-      //condition to check if use has selected an item
-      if (key === "Enter") {
-        //open the config menu for the element if user clicked enter
-        setActiveItem(selectedItem);
-        setOpenConfigurePopup(true);
-      } else if (key === "Delete") {
-        //delete item from canvas if user clicked delete
-        setActiveItem({});
-        setSelectedItem({});
-        setItems(items.filter((item) => item.Id !== selectedItem.Id));
-      }
+    //condition to check if user has selected an item
+    if (selectedItems.length === 1 && key === "Enter") {
+      //open the config menu for the element if user clicked enter
+      //can't open the menu for multiple elements
+      setActiveItem(selectedItems[0]);
+      setOpenConfigurePopup(true);
+    } else if (selectedItems.length > 1 && key === "Delete") {
+      //delete item from canvas if user clicked delete
+      //can delete multiple items at one time
+      setActiveItem({});
+      setSelectedItems([]);
+      const itemIdsToDelete = selectedItems.map((a) => a.Id);
+      setItems(items.filter((item) => !itemIdsToDelete.includes(item.Id)));
     }
+  };
+
+  //DRAGGING AND SELECTING EVENTS
+  const handleMouseDown = (e) => {
+    //first event that runs when user begins to select items
+    setIsDragging(true);
+    const canvasRect = canvasRef.current.getBoundingClientRect();
+    setSelectionArea({
+      start: { x: e.clientX - canvasRect.left, y: e.clientY - canvasRect.top },
+      end: { x: e.clientX - canvasRect.left, y: e.clientY - canvasRect.top },
+    }); //getting the cooridnate of first click
+  };
+
+  const handleMouseMove = (e) => {
+    //second event that fires when user moves mouse
+    if (isDragging) {
+      // check if user is still pressing the mouse
+      //only relevant if user is still pressing and dragging
+      const canvasRect = canvasRef.current.getBoundingClientRect();
+      setSelectionArea({
+        start: selectionArea.start,
+        end: { x: e.clientX - canvasRect.left, y: e.clientY - canvasRect.top },
+      }); //keep updating the selection area; important to display the selection area in UI
+    }
+  };
+
+  const handleMouseUp = (e) => {
+    //final event when user lifts off the click
+
+    const newSelectedItems = items.filter(
+      (item) => isItemInSelectionArea(item, selectionArea) // |filter out selected items in the selection area
+    );
+    newSelectedItems.length
+      ? setSelectedItems(newSelectedItems)
+      : setSelectedItems([]);
+    setIsDragging(false); //drag and select is complete
+  };
+
+  const isItemInSelectionArea = (item, area) => {
+    const itemRect = {
+      left: item.position.x,
+      top: item.position.y,
+      right: item.position.x,
+      bottom: item.position.y,
+    };
+
+    return (
+      itemRect.left >= area.start.x &&
+      itemRect.top >= area.start.y &&
+      itemRect.right <= area.end.x &&
+      itemRect.bottom <= area.end.y
+    ); //check if item corridnates are inside the selection rectangle area
   };
 
   return (
     <React.Fragment>
-      <div className="canvas" onClick={() => setSelectedItem({})}>
-        {/* <div className="canvas-header">CANVAS</div> */}
+      <div className="canvas">
         <div
           className="canvas-area"
           onDragOver={(e) => e.preventDefault()}
           onDrop={(e) => handleDrop(e)}
+          onMouseDown={(e) => handleMouseDown(e)} //click
+          onMouseMove={(e) => handleMouseMove(e)} //drag and move
+          onMouseUp={(e) => handleMouseUp(e)} //finish selection
+          ref={canvasRef}
         >
-          {items.length
-            ? items.map((item, i) => (
-                <div
-                  tabIndex={i}
-                  key={`Item-${i}`}
-                  draggable
-                  className="dragged-item"
-                  style={{
-                    outline: "none",
-                    transform: `translate(${item.position.x}px, ${item.position.y}px`,
-                    fontWeight: item.weight,
-                    fontSize: item.size + "px",
-                    border: selectedItem.Id === item.Id ? "1px solid red" : "",
-                  }}
-                  onDragStart={(e) =>
-                    setDraggedItem({
-                      ...item,
-                      x:
-                        e.clientX -
-                        e.currentTarget.getBoundingClientRect().left,
-                      y:
-                        e.clientY - e.currentTarget.getBoundingClientRect().top, //calculating the offset
-                    })
-                  } //item being dragged
-                  onDoubleClick={() => {
-                    setActiveItem(item);
-                    setOpenConfigurePopup(true);
-                  }}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    selectedItem.Id === item.Id
-                      ? setSelectedItem({})
-                      : setSelectedItem(item);
-                  }}
-                  onKeyDown={(e) => handleKeyDown(e.key)}
-                >
-                  {item.name}
-                </div>
-              ))
-            : null}
+          {items &&
+            items.map((item, i) => (
+              <div
+                tabIndex={i}
+                key={`Item-${i}`}
+                draggable
+                className="dragged-item"
+                style={{
+                  outline: "none",
+                  transform: `translate(${item.position.x}px, ${item.position.y}px`,
+                  fontWeight: item.weight,
+                  fontSize: item.size + "px",
+                  color: item.color,
+                  background: item.background,
+                  border: selectedItems.some((a) => a.Id === item.Id)
+                    ? "1px solid red"
+                    : "",
+                }}
+                onDragStart={(e) =>
+                  setDraggedItem({
+                    ...item,
+                    x: e.clientX - e.currentTarget.getBoundingClientRect().left,
+                    y: e.clientY - e.currentTarget.getBoundingClientRect().top, //calculating the offset
+                  })
+                } //item being dragged
+                onDoubleClick={(e) => {
+                  e.stopPropagation();
+                  setActiveItem(item);
+                  setOpenConfigurePopup(true); //open the config menu on double click
+                }}
+                onClick={(e) => setSelectedItems([item])}
+                onKeyDown={(e) => handleKeyDown(e.key)}
+              >
+                {item.type === "Input" ? <input type="text" /> : item.name}
+              </div>
+            ))}
         </div>
       </div>
+
+      {isDragging && (
+        <div
+          className="selection-area"
+          style={{
+            left: `${selectionArea.start.x}px`,
+            top: `${selectionArea.start.y}px`,
+            width: `${selectionArea.end.x - selectionArea.start.x}px`,
+            height: `${selectionArea.end.y - selectionArea.start.y}px`,
+          }}
+        /> //display selection area if the user is still dragging
+      )}
 
       <ConfigureElement
         openPopup={openConfigurePopup}
